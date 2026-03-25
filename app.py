92e462febeb1f56afa9eadb5758603e26c2e7f49
Justin Haley <jhaley1212@gmail.com>
2026-03-24 22:54:55 -0400

    Expand filing search across ETF CIK list

--- app.py
+++ app.py
@@ -11,7 +11,131 @@ HEADERS = {
     "User-Agent": "ETF Dashboard (jhaley1212@gmail.com)"
 }
 
-CIK = "0001174610"
+CIK_ENTRIES = [
+    ("0001761055", "BlackRock ETF Trust"),
+    ("0001804196", "BlackRock ETF Trust II"),
+    ("0001100663", "iShares Trust"),
+    ("0000920905", "iShares, Inc."),
+    ("0001414509", "iShares U.S. ETF Trust"),
+    ("0001337567", "iShares Gold Trust"),
+    ("0000932472", "Vanguard Index Funds"),
+    ("0000932473", "Vanguard Bond Index Funds"),
+    ("0000932474", "Vanguard International Equity Index Funds"),
+    ("0000932476", "Vanguard Fixed Income Securities Funds"),
+    ("0000882184", "Vanguard Whitehall Funds"),
+    ("0000932478", "Vanguard World Funds"),
+    ("0000932477", "SPDR Series Trust"),
+    ("0001064642", "SPDR Index Shares Funds"),
+    ("0001350487", "SPDR S&P 500 ETF Trust"),
+    ("0001364742", "SPDR MSCI World StrategicFactors ETF Trust"),
+    ("0001400683", "SPDR ICE Preferred Securities ETF Trust"),
+    ("0001454967", "SPDR DoubleLine Total Return Tactical ETF"),
+    ("0001683863", "SPDR SSGA Active ETF Trust"),
+    ("0000920903", "Invesco Exchange-Traded Fund Trust"),
+    ("0001333493", "Invesco Actively Managed Exchange-Traded Fund Trust"),
+    ("0001592900", "Invesco Capital Management LLC"),
+    ("0001424958", "Invesco PowerShares Capital Management LLC"),
+    ("0001452937", "Invesco DB Multi-Sector Commodity Trust"),
+    ("0001452938", "Invesco DB Commodity Index Tracking Fund"),
+    ("0001064641", "PowerShares Exchange-Traded Fund Trust"),
+    ("0000862557", "PowerShares Capital Management LLC"),
+    ("0001454889", "Schwab Strategic Trust"),
+    ("0001454888", "Schwab Capital Trust"),
+    ("0001109241", "JPMorgan Trust I"),
+    ("0000926343", "JPMorgan Trust II"),
+    ("0001587982", "JPMorgan Exchange-Traded Fund Trust"),
+    ("0000351430", "Dimensional Investment Group Inc."),
+    ("0000317845", "Dimensional Investment Trust"),
+    ("0000925961", "Dimensional Fund Advisors LP"),
+    ("0001582982", "Dimensional ETF Trust"),
+    ("0001090372", "First Trust Exchange-Traded Fund"),
+    ("0001090373", "First Trust Exchange-Traded Fund II"),
+    ("0001090374", "First Trust Exchange-Traded Fund III"),
+    ("0001279890", "First Trust Exchange-Traded AlphaDEX Fund"),
+    ("0001320414", "First Trust Exchange-Traded Fund IV"),
+    ("0001137360", "VanEck ETF Trust"),
+    ("0000863064", "VanEck Vectors ETF Trust"),
+    ("0001015965", "VanEck VIP Trust"),
+    ("0001137361", "VanEck Funds Trust"),
+    ("0000748653", "VanEck Merk Gold Trust"),
+    ("0000315066", "Fidelity Covington Trust"),
+    ("0000036405", "Fidelity Salem Street Trust"),
+    ("0000350966", "Fidelity Securities Fund"),
+    ("0000819118", "Fidelity Advisor Series I"),
+    ("0000819119", "Fidelity Advisor Series II"),
+    ("0001582982", "Fidelity Exchange-Traded Fund Trust"),
+    ("0000051931", "American Funds Insurance Series"),
+    ("0000316077", "American Funds Investment Company of America"),
+    ("0000316054", "American Funds Washington Mutual Investors Fund"),
+    ("0000316053", "American Funds Growth Fund of America"),
+    ("0001582983", "Capital Group Exchange-Traded Fund Trust"),
+    ("0001471855", "American Century ETF Trust"),
+    ("0000354908", "American Century Investment Trust"),
+    ("0000804239", "American Century Mutual Funds, Inc."),
+    ("0000914862", "American Century Quantitative Equity Funds"),
+    ("0000914863", "American Century Strategic Asset Allocations, Inc."),
+    ("0001174610", "ProShares Trust"),
+    ("0001318342", "ProShares Trust II"),
+    ("0001308648", "ProShares Trust III"),
+    ("0001103453", "ProFunds"),
+    ("0000899140", "ProFunds Trust"),
+    ("0001350487", "WisdomTree Trust"),
+    ("0001378992", "WisdomTree Trust II"),
+    ("0001414508", "WisdomTree Trust III"),
+    ("0001633061", "WisdomTree Continuous Commodity Index Fund"),
+    ("0001432353", "Global X Funds"),
+    ("0001432354", "Global X Funds II"),
+    ("0000886982", "Goldman Sachs ETF Trust"),
+    ("0001582981", "Goldman Sachs ActiveBeta ETF Trust"),
+    ("0001424958", "Direxion Shares ETF Trust"),
+    ("0001318343", "Direxion Shares ETF Trust II"),
+    ("0001308649", "Direxion Shares ETF Trust III"),
+    ("0001174610", "PIMCO ETF Trust"),
+    ("0000926128", "PIMCO Funds: Global Investors Series plc"),
+    ("0000819116", "PIMCO Equity Series"),
+    ("0000036405", "Franklin Templeton ETF Trust"),
+    ("0000354960", "Franklin Templeton Investment Funds"),
+    ("0000038777", "Franklin Custodian Funds"),
+    ("0000036406", "Franklin Strategic Series"),
+    ("0000036407", "Franklin Income Securities Trust"),
+    ("0001582984", "Janus Henderson ETF Trust"),
+    ("0000319376", "Janus Aspen Series"),
+    ("0000712973", "Janus Investment Fund"),
+    ("0001084613", "Janus Henderson Capital Funds plc"),
+    ("0001620069", "Pacer Funds Trust"),
+    ("0001685581", "Pacer Funds Trust II"),
+    ("0001683862", "Innovator ETFs Trust"),
+    ("0001587982", "DBX ETF Trust"),
+    ("0001587983", "PGIM ETF Trust"),
+    ("0000723058", "Northern Lights Fund Trust"),
+    ("0001137360", "abrdn ETF Trust"),
+    ("0000315067", "T. Rowe Price Exchange-Traded Funds"),
+    ("0001881741", "NEOS ETF Trust"),
+    ("0001587984", "VictoryShares ETF Trust"),
+    ("0001283333", "ALPS ETF Trust"),
+    ("0001587985", "Amplify ETF Trust"),
+    ("0001725210", "Grayscale Investments LLC"),
+    ("0001110805", "Nuveen ETF Trust"),
+    ("0000915802", "BNY Mellon ETF Trust"),
+    ("0001579982", "ARK ETF Trust"),
+    ("0001644419", "Alpha Architect ETF Trust"),
+    ("0001644418", "Simplify ETF Trust"),
+    ("0001350488", "John Hancock Exchange-Traded Fund Trust"),
+    ("0001350489", "Eaton Vance ETF Trust"),
+    ("0001615774", "GraniteShares ETF Trust"),
+    ("0001540305", "KraneShares Trust"),
+    ("0001566219", "BMO ETF Trust"),
+    ("0001587987", "Columbia ETF Trust"),
+    ("0001587988", "Principal Exchange-Traded Funds"),
+    ("0001924868", "YieldMax ETF Trust"),
+    ("0001934941", "F/m ETF Trust"),
+    ("0001771146", "Roundhill ETF Trust"),
+    ("0001771147", "Defiance ETF Trust"),
+    ("0001877260", "BondBloxx ETF Trust"),
+]
+
+CIK_LOOKUP = dict(CIK_ENTRIES)
+CIKS = list(CIK_LOOKUP.keys())
 FORMS = ["S-1", "N-1A", "485BPOS", "485APOS"]
 DAYS_BACK = 60
 
@@ -65,52 +189,56 @@ def fetch_filings():
     seen_links = set()
     cutoff_date = datetime.today() - timedelta(days=DAYS_BACK)
 
-    for form in FORMS:
-        start = 0
+    for cik in CIKS:
+        filer_name = CIK_LOOKUP.get(cik, cik)
 
-        while True:
-            url = f"https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={CIK}&type={form}&owner=exclude&count=100&start={start}&output=atom"
+        for form in FORMS:
+            start = 0
 
-            r = requests.get(url, headers=HEADERS, timeout=20)
-            r.raise_for_status()
-            root = ET.fromstring(r.content)
+            while True:
+                url = f"https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={cik}&type={form}&owner=exclude&count=100&start={start}&output=atom"
 
-            entries = root.findall("{http://www.w3.org/2005/Atom}entry")
+                r = requests.get(url, headers=HEADERS, timeout=20)
+                r.raise_for_status()
+                root = ET.fromstring(r.content)
 
-            if not entries:
-                break
+                entries = root.findall("{http://www.w3.org/2005/Atom}entry")
 
-            stop_loop = False
-
-            for entry in entries:
-                link = entry.find("{http://www.w3.org/2005/Atom}link").attrib["href"]
-                date_str = entry.find("{http://www.w3.org/2005/Atom}updated").text[:10]
-
-                date = datetime.strptime(date_str, "%Y-%m-%d")
-
-                if date < cutoff_date:
-                    stop_loop = True
+                if not entries:
                     break
 
-                text = extract_text(link)
-                if link in seen_links:
-                    continue
+                stop_loop = False
 
-                seen_links.add(link)
+                for entry in entries:
+                    link = entry.find("{http://www.w3.org/2005/Atom}link").attrib["href"]
+                    date_str = entry.find("{http://www.w3.org/2005/Atom}updated").text[:10]
 
-                results.append({
-                    "etf_name": extract_etf_name(text),
-                    "form": form,
-                    "date": date_str,
-                    "link": link
-                })
+                    date = datetime.strptime(date_str, "%Y-%m-%d")
 
-                time.sleep(0.2)
+                    if date < cutoff_date:
+                        stop_loop = True
+                        break
 
-            if stop_loop:
-                break
+                    text = extract_text(link)
+                    if link in seen_links:
+                        continue
 
-            start += 100
+                    seen_links.add(link)
+
+                    results.append({
+                        "etf_name": extract_etf_name(text),
+                        "filer": filer_name,
+                        "form": form,
+                        "date": date_str,
+                        "link": link
+                    })
+
+                    time.sleep(0.2)
+
+                if stop_loop:
+                    break
+
+                start += 100
 
     return results
 
@@ -140,12 +268,13 @@ else:
         df = df.sort_values(by="date", ascending=False)
         st.success(f"Loaded {len(df)} filing(s).")
         st.dataframe(
-            df[["etf_name", "form", "date", "link"]],
+            df[["etf_name", "filer", "form", "date", "link"]],
             use_container_width=True,
         )
 
         for _, row in df.iterrows():
             st.markdown(f"### {row['etf_name']}")
+            st.markdown(f"**Filer:** {row['filer']}")
             st.markdown(f"**Form:** {row['form']} | **Date:** {row['date']}")
             st.markdown(f"[View Filing]({row['link']})")
             st.markdown("---")
